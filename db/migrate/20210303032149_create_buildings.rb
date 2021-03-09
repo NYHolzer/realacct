86edffc05ac240c76da4599effb703e93fe81873
NYHolzer <nissan.holzer@gmail.com>
2021-03-08 21:10:53 -0500

    building migration

---
+++ db/migrate/20210303032149_create_buildings.rb
@@ -0,0 +1,14 @@
+class CreateBuildings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :buildings do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.blob :image
+
+      t.timestamps
+    end
+  end
+end
